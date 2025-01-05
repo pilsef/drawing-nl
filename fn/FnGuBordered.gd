77c34ed43481ff92207101c738b4898ed524f801
pilsef <jager.will@gmail.com>
2025-01-04 16:03:37 -0800

    allow negative border sizes for inner borders

--- fn/FnGuBordered.gd
+++ fn/FnGuBordered.gd
@@ -9,13 +9,14 @@ class_name FnGuBordered
 func exec():
 	
 	var gu_base:Guber = rq_gu.exec()
+	var offset_border = max(0, width)
 	
-	var rect_border = gu_base.bounds.grow(width)
+	var rect_border = gu_base.bounds.grow(offset_border)
 	rect_border.position = Vector2.ZERO
 	
 	var size = rect_border.size
-	var shrink_h = -size.x + width
-	var shrink_v = -size.y + width
+	var shrink_h = -size.x + abs(width)
+	var shrink_v = -size.y + abs(width)
 	
 	var img = Image.create_empty(size.x, size.y, false, Image.FORMAT_RGBA8)
 	img.fill_rect(rect_border.grow_side(SIDE_RIGHT, shrink_h), color)
@@ -27,6 +28,7 @@ func exec():
 	rq_gu_border.tex = ImageTexture.create_from_image(img)
 	
 	var gu_border:Guber = rq_gu_border.exec()
-	gu_border.place(gu_base, Vector2(width, width), Dir.DOWN_RIGHT)
 	
-	return gu_border
+	var pos_border = gu_base.bounds.position + Vector2(-offset_border, -offset_border)
+	gu_base.place(gu_border, pos_border, Dir.DOWN_RIGHT)
+	return gu_base
